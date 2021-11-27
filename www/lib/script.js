c8018b190b62a8c52686d9daa1eaf73792c653bd
CarinaNovais <carinapereiraN@outlook.com>
2021-11-27 10:31:57 +0900

    add plugin de geolocalização

--- www/lib/script.js
+++ www/lib/script.js
@@ -1,15 +1,37 @@
 // This is a JavaScript file
 window.onload = function(){
 
-  document.querySelector("#local").addEventListener("click", function(){
-        L.mapquest.key = 'WttG3ae5icu36vH6g1qUIV18qm2KLJ3G';
+function mapa(lat,long){
+   L.mapquest.key = 'WttG3ae5icu36vH6g1qUIV18qm2KLJ3G';
 
         var map = L.mapquest.map('map', {
-          center: [37.7749, -122.4194],
+          center: [lat, long],
           layers: L.mapquest.tileLayer('map'),
           zoom: 12
         });
         
         map.addControl(L.mapquest.control());
-    });
+    };
 }
+
+  document.querySelector("#local").addEventListener("click", function(){
+
+    // onSuccess Callback
+    // This method accepts a Position object, which contains the
+    // current GPS coordinates
+    //
+    var onSuccess = function(position) {
+        mapa(position.coords.latitude, position.coords.longitude);
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  });
+
+  
